e20236a39202729e28580769b0916889bf6dd0a9
Abhishek W3Dev <abhishek.k@w3dev.email>
2024-02-29 16:33:48 +0530

    testphase 2pass_68

--- testfile/controllers/googleLoginNext.js
+++ testfile/controllers/googleLoginNext.js
@@ -7,9 +7,9 @@ const sleep = require('../../utils/sleep.js')
 const Next_GoogleLogintHandler = async (page) => {
     const loginButton= 'a[class="text-xs md:text-base px-2 py-1 border-none text-black "]'
     await page.click(loginButton)
-    await sleep(5000)
+    await sleep(8000)
     await page.click('button[class="my-2 w-max bg-white border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-red-300 focus:outline-none text-gray-800 text-sm font-medium px-4 py-2 rounded flex items-center space-x-2 transition-colors duration-200"]')
-    await sleep(10000)
+    await sleep(28000)
     const newUrl = page.url();
     console.log(' > Validating the URL for correct navigation')
   // Verify if the navigation was successful
@@ -24,13 +24,15 @@ const Next_GoogleLogintHandler = async (page) => {
         console.log("  > Navigating to the Login page again")
         await page.goto('https://next.appreviewbot.com/login/')
         
+        
         await page.click('button[class="my-2 w-max bg-white border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-red-300 focus:outline-none text-gray-800 text-sm font-medium px-4 py-2 rounded flex items-center space-x-2 transition-colors duration-200"]')
-
+        await sleep(28000)
+        const secondnewUrl = page.url();
 
         console.log('   > Second attempt: Email entered ')
          
         await sleep(8000)
-        if(newUrl.includes('https://next.appreviewbot.com/')) {
+        if(secondnewUrl.includes('https://next.appreviewbot.com/')) {
             console.log('   > Button click navigated to the correct page.');
             await sleep(2000)
             console.log('   > Second Attempt: Login successful')
