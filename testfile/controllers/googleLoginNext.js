113e74196ee64bd0ed539d4ec7f542a03b20dd05
Abhishek W3Dev <abhishek.k@w3dev.email>
2024-02-29 17:58:23 +0530

    testphase 2pass_72

--- testfile/controllers/googleLoginNext.js
+++ testfile/controllers/googleLoginNext.js
@@ -13,23 +13,31 @@ const Next_GoogleLogintHandler = async (page) => {
     console.log('  > Login with google clicked')
     await sleep(60000)
     const newUrl = page.url();
+    console.log('Navigated page url',newUrl)
     console.log(' > Validating the URL for correct navigation')
   // Verify if the navigation was successful
     if (newUrl === 'https://next.appreviewbot.com/') {
-        console.log('  > Navigated to the correct page.');
+        console.log('  > Navigated to the correct page.', newUrl);
         await sleep(2000)
         console.log('  > Login successful')
+        // Take a screenshot if the condition is not met
+        await page.screenshot({path: 'pass_screenshot1.png', fullPage: true});
+        console.log('Screenshot taken and saved as failure_screenshot.png');
     } 
     else {
         console.log('  > Not navigated to the expected page.');
-        await sleep(2000)
+        await sleep(5000)
+        // Take a screenshot if the condition is not met
+        await page.screenshot({path: 'arbfailure_screenshot1.png', fullPage: true});
+        console.log('Screenshot taken and saved as failure_screenshot.png');
         console.log("  > Navigating to the Login page again")
         await page.goto('https://next.appreviewbot.com/login/')
 
-        
+        await sleep(8000)
         await page.click('button[class="my-2 w-max bg-white border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-red-300 focus:outline-none text-gray-800 text-sm font-medium px-4 py-2 rounded flex items-center space-x-2 transition-colors duration-200"]')
-        await sleep(20000)
+        await sleep(30000)
         const secondnewUrl = page.url();
+        console.log('navigated URL at second time',secondnewUrl)
 
         console.log('   > Second attempt: Email entered ')
          
@@ -38,9 +46,18 @@ const Next_GoogleLogintHandler = async (page) => {
             console.log('   > Button click navigated to the correct page.');
             await sleep(2000)
             console.log('   > Second Attempt: Login successful')
+            // Take a screenshot if the condition is not met
+        await page.screenshot({path: 'pass_screenshot2.png', fullPage: true});
+        console.log('Screenshot taken and saved as failure_screenshot.png');
          
         }else{
-        console.log("Try again with correct URL") 
+
+        // Take a screenshot if the condition is not met
+            await page.screenshot({path: 'arbfailure_screenshot2.png', fullPage: true});
+            console.log('Screenshot taken and saved as failure_screenshot.png');
+            console.log("Try again with correct URL") 
+
+
         }
 
     }
